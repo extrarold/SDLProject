65b294c41fcb4a6489e4e2560f060ab872e66e64
Harold Heim <extrarold@users.noreply.github.com>
2015-05-11 22:53:18 +0200

    Update player.c

--- player.c
+++ player.c
@@ -191,7 +191,7 @@ void fight(GameObject *player1, GameObject *player2, Input *input1, Input *input
 
     }
     
-    if(victoires == 0) {
+    if(victoires == 3) {
         setPlayer1wins((drawMessage("Player 1 WINS", 255, 0, 0)));
         
     } else if(victoires == -3) {
